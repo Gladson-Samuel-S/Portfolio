4dbc154481aa995b88b08d8c2628cc016c198db5
Gladson-Samuel-S <samuelgladson4@gmail.com>
2022-09-21 23:00:31 +0530

    Changed Site SEO

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'Sharing my thoughts with you lot :)',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://gladsonsamuel.vercel.app/',
+  siteRepo: 'https://github.com/Gladson-Samuel-S/Portfolio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
